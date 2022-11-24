8b0aec1607057549952d855ae8c62aedb04a64a0
FHP187 <gjimenez@jaumebalmes.net>
2022-11-24 20:33:55 +0100

    5st commit connect python

--- connection.py
+++ connection.py
@@ -1,1 +1,9 @@
-conn = psycopg2.connect("dbname=postgres user=postgresUser password=postgresPW")
+# conn = psycopg2.connect("dbname=postgres user=postgresUser password=postgresPW")
+
+import psycopg2
+
+conn = psycopg2.connect(database="postgres",
+                        host="localhost",
+                        user="postgresUser",
+                        password="postgresPW",
+                        port="5455")
